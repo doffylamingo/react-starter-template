ce673809c68355158d095be058d643dc7c7402f6
doffylamingo <102771222+doffylamingo@users.noreply.github.com>
2025-01-25 14:49:47 +0800

    Add TanStack Router

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="text-5xl font-bold text-red-900">Hello, World!</div>;
-}
-
-export default App;
---
+++ src/routes/__root.tsx
@@ -0,0 +1,30 @@
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <>
+      <div className="flex gap-2 p-2">
+        <Link
+          className="[&.active]:font-bold"
+          to="/"
+        >
+          Home
+        </Link>{" "}
+        <Link
+          className="[&.active]:font-bold"
+          to="/about"
+        >
+          About
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  );
+}
